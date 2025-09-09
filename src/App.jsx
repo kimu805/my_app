42bf3de175cc20ce5f9f3b08a7f7418057c04050
kimu805 <kimukimuok805@gmail.com>
2025-09-09 10:31:14 +0900

    commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+import { ColoredMessage } from "./components/ColoredMEssage"
+
+export const App = () => {
+  console.log("レンダリング")
+  const [num, setNum] = useState(0)
+
+  const onClickButton = () => {
+    setNum(num + 1)
+  }
+
+  return (
+    <>
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <ColoredMessage color="blue">お元気ですか？</ColoredMessage>
+      <ColoredMessage color="pink">元気です！</ColoredMessage>
+      <button onClick={onClickButton}>ボタン</button>
+      <p>{num}</p>
+    </>
+  )
+}
